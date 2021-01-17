1368f5a697b5dc706c8296a65a45d8e5f0406a87
stancangit <51001942+stancangit@users.noreply.github.com>
2021-01-17 11:59:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ sudo gcc main.c
 sudo mkdir /usr/bin/.gitpkg
 sudo mv a.out /usr/bin/.gitpkg/gitpkg_run
 sudo chmod +x /usr/bin/.gitpkg/gitpkg_run
-sudo mv gitpkg /usr/bin/gitpkg
+sudo cp gitpkg /usr/bin/gitpkg
 sudo chmod +x /usr/bin/gitpkg
 
