aaeab562ff53edb6fc66cc804257fcd275ba24f2
stasiop <51001942+stasiop@users.noreply.github.com>
2021-01-17 21:32:50 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo gcc main.c
 sudo mkdir /usr/bin/.gitpkg
+sudo mkdir /tmp/gitpkg
 sudo mv a.out /usr/bin/.gitpkg/gitpkg_run
 sudo chmod +x /usr/bin/.gitpkg/gitpkg_run
 sudo cp gitpkg /usr/bin/gitpkg
